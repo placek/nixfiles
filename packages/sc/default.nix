c5b3218e5e42aec5aed15aa66d4d0629162c3753
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-12-31 11:55:17 +0100

    move sc to external repo

--- packages/sc/default.nix
+++ packages/sc/default.nix
@@ -2,14 +2,19 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "sc-${version}";
-  version = "1.0";
+  version = "0mrqq9";
 
-  src = ../../sources/sc;
+  src = pkgs.fetchFromGitHub {
+    owner  = "placek";
+    repo   = "sc";
+    rev    = "c4b1f01cdfc178cb98caff7b064af95f566c9914";
+    sha256 = "0mrqq9a1zpmr876gr16fdjf1gvfzmr8gh33rkj6nc4rr0793ydph";
+  };
 
+  buildInputs = [];
   buildPhase = "";
   installPhase = ''
-    mkdir -p $out/bin
-    cp $src/* $out/bin
-    chmod +x $out/bin/*
+    mkdir -p $out
+    cp -r * $out
   '';
 }
