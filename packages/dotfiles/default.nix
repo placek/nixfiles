d99bc6adceaf2ce9f0116aacce73139132a17098
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-12-31 17:50:47 +0100

    fix issues with dotfiles

--- packages/dotfiles/default.nix
+++ packages/dotfiles/default.nix
@@ -2,19 +2,18 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "dotfiles-${version}";
-  version = "";
+  version = "14hq4z";
 
   src = pkgs.fetchFromGitHub {
     owner  = "placek";
     repo   = "dotfiles";
-    rev    = "b63aa9ce51d8a46fa0405a70ca5bd75039fad26a";
-    sha256 = "1956vqjv3qm1qbrajds7h835j86znxnhq0pjdglpgidclrvi4xwd";
+    rev    = "26e879a6816575ce3bf16bb402555c25e0a20b58";
+    sha256 = "14hq4zc4g4v31hx82k3hv10sgjjr5z2xl0d2x8nqpm77rfzf0qi0";
   };
 
-  buildInputs = [ pkgs.stow ];
+  buildInputs = [];
   buildPhase = "";
   installPhase = ''
-    sed -i 's#^stow=.*$#stow="${pkgs.stow}/bin/stow"#' bin/dots
     mkdir -p $out
     cp -r * $out
   '';
