29c240462b1b3547e836fdf41db778b0cd7a7daf
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-12-31 11:32:46 +0100

    move projects to external repo

--- packages/projects/default.nix
+++ packages/projects/default.nix
@@ -2,14 +2,19 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "projects-${version}";
-  version = "1.2";
+  version = "0f8b27";
 
-  src = ../../sources/projects;
+  src = pkgs.fetchFromGitHub {
+    owner  = "placek";
+    repo   = "projects";
+    rev    = "0f8b27c61f602b87b08bd3b5865e79bef130c3f2";
+    sha256 = "0dkz9l2ikpv62r1yjnq318cd5s0jci9hsv7f0r5madw1zn33dj8l";
+  };
 
+  buildInputs = [];
   buildPhase = "";
   installPhase = ''
-    mkdir -p $out/bin
-    cp $src/* $out/bin
-    chmod +x $out/bin/*
+    mkdir -p $out
+    cp -r * $out
   '';
 }
