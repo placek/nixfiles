5eccd9a2bafac6f6724efe0179a307edf34ea01b
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-01-07 10:41:00 +0100

    add ntfs support

--- roles/common/default.nix
+++ roles/common/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   boot.cleanTmpDir                         = true;
+  boot.supportedFilesystems                = [ "ntfs" ];
   console.keyMap                           = "pl";
   i18n.defaultLocale                       = "pl_PL.UTF-8";
   networking.firewall.allowPing            = false;
