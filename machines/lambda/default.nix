5a1239147274f408178ca8a4bac9e5b718c0cfee
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-01-08 13:25:04 +0100

    possibly better ATI radeon support

--- machines/lambda/default.nix
+++ machines/lambda/default.nix
@@ -11,23 +11,27 @@
     ../../users/placek.nix
   ];
 
+  boot.initrd.kernelModules                  = [ "amdgpu" ];
+  boot.kernelPackages                        = pkgs.linuxPackages_5_4;
   boot.loader.efi.canTouchEfiVariables       = true;
   boot.loader.systemd-boot.enable            = true;
-  boot.initrd.kernelModules                  = [ "amdgpu" ];
   hardware.bluetooth.enable                  = true;
+  hardware.enableRedistributableFirmware     = true;
   hardware.facetimehd.enable                 = true;
+  hardware.opengl.driSupport                 = true;
+  hardware.opengl.enable                     = true;
   hardware.opengl.extraPackages              = [ pkgs.vaapiIntel ];
   hardware.pulseaudio.enable                 = true;
   networking.hostName                        = "lambda";
   programs.light.enable                      = true;
+  services.mbpfan.enable                     = true;
+  services.mbpfan.highTemp                   = 64;
+  services.mbpfan.lowTemp                    = 61;
+  services.mbpfan.maxTemp                    = 84;
   services.xserver.libinput.enable           = true;
   services.xserver.libinput.naturalScrolling = true;
   services.xserver.libinput.scrollMethod     = "twofinger";
   services.xserver.libinput.tapping          = false;
-  services.mbpfan.enable                     = true;
-  services.mbpfan.lowTemp                    = 61;
-  services.mbpfan.highTemp                   = 64;
-  services.mbpfan.maxTemp                    = 84;
 
   users.users.placek.packages = with pkgs; [
     arduino
