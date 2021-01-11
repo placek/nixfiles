6c57cf83ea36f9fd033f8864be2f256bd0406a5b
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-01-11 20:47:01 +0100

    fix fish colors

--- packages/dotfiles/default.nix
+++ packages/dotfiles/default.nix
@@ -2,13 +2,13 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "dotfiles-${version}";
-  version = "16778e";
+  version = "beaeac";
 
   src = pkgs.fetchFromGitHub {
     owner  = "placek";
     repo   = "dotfiles";
-    rev    = "16778e9421d2b6b3bb3b4434e585c13fc33330d3";
-    sha256 = "057q8rza9li113r7l8qfi63l9vpacq4l8wj2204v4l7q2rhacxiy";
+    rev    = "beaeac438c72d92ce1ed95b93fc42ecb9dc1c2b9";
+    sha256 = "0dgyxl52wgf2d4rmw06xay5n64z4kfcaa6rz92shws9hy1apkqd4";
   };
 
   buildInputs = [ pkgs.haskellPackages.mustache ];
