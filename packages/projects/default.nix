cc18db64b15c154f68cb43e8374920838f237505
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-01-21 11:39:55 +0100

    bump version of projects

--- packages/projects/default.nix
+++ packages/projects/default.nix
@@ -2,13 +2,13 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "projects-${version}";
-  version = "1dzxfx";
+  version = "a7da55";
 
   src = pkgs.fetchFromGitHub {
     owner  = "placek";
     repo   = "projects";
-    rev    = "c036d7808aff176f5052fbc727ccf52d3ebea5f9";
-    sha256 = "1dzxfxspbb45dwp0sji0dlzjaj92k7frjyg68gyr14qvg7vq6dl4";
+    rev    = "a7da55bd10f1d0459893de0fad1a83c2139eeccc";
+    sha256 = "0f792djcr2f8bl5mcgh8y4s4cf45q1v100xj1pbp44aiymkcz1ld";
   };
 
   buildInputs = [];
