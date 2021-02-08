1b286feec27d12f3b429bf498f11ef9a46d3e22f
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-02-08 17:07:55 +0100

    bump dotfiles version

--- packages/dotfiles/default.nix
+++ packages/dotfiles/default.nix
@@ -2,13 +2,13 @@
 
 pkgs.stdenv.mkDerivation rec {
   name    = "dotfiles-${version}";
-  version = "994117";
+  version = "f2e621";
 
   src = pkgs.fetchFromGitHub {
     owner  = "placek";
     repo   = "dotfiles";
-    rev    = "9941178ae37bd3fff9d845d97774bd4d7d14c9ef";
-    sha256 = "16b8nkf3sg37r74mwx8719pgqc3xw3rgvc093pkphdnfy1n6bcdi";
+    rev    = "f2e62138bd2707c8bc3cc9ca92cf7fd3037ffb3e";
+    sha256 = "00sf47x77rzrh603dmlvfv6yyqlgb3bd0h2xnflkximfjhxh567y";
   };
 
   buildInputs  = [ pkgs.haskellPackages.mustache ];
