629de9e8998421bc998130499b10f2af65083501
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-03-16 22:06:12 +0100

    bump version of git-shell-commands

--- packages/git-shell-commands/default.nix
+++ packages/git-shell-commands/default.nix
@@ -2,13 +2,13 @@
 
 pkgs.stdenv.mkDerivation rec {
   name    = "git-shell-commands-${version}";
-  version = "69adb2";
+  version = "c948b8";
 
   src = pkgs.fetchFromGitHub {
     owner  = "placek";
     repo   = "git-shell-commands";
-    rev    = "69adb2564fefb76e369b9e1649c235f5c19a4326";
-    sha256 = "0r1d8nf3vx5kvy358lbznly8r37wcgf1wa73alcrdl485lsgr4rr";
+    rev    = "c948b898168f05391456835d7bdffc3a0f28e21f";
+    sha256 = "1r41f8phs2w9abqas6hvg43m56hc6d8i0q6lqbia4r853ziw2pgm";
   };
 
   buildInputs  = [];
