52321ba78c3cf07280ec300de2bf9153bcd7e0b9
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-06-07 08:51:09 +0200

    changes to secret device

--- hardware/usb/secret/default.nix
+++ hardware/usb/secret/default.nix
@@ -3,17 +3,17 @@
 {
   fileSystems."/mnt/secret1" = {
     # to open disk:
-    #   sudo cryptsetup open /dev/disk/by-uuid/19b73b1a-c557-4981-aeb9-832bd0a02268 secret
-    #   mount /mnt/private1
+    #   sudo cryptsetup open /dev/disk/by-uuid/e2948dc0-c2ff-4425-bc1b-27dd2ac6674a secret
+    #   mount /mnt/secret1
     # to close disk:
-    #   umount /mnt/private1
+    #   umount /mnt/secret1
     #   sudo cryptsetup close secret
     device = "/dev/mapper/secret";
     fsType = "auto";
     options = [ "defaults" "user" "rw" "noauto" ];
   };
   fileSystems."/mnt/secret2" = {
-    device = "/dev/disk/by-uuid/5a43262d-2451-4302-9b40-b359e4162dcb";
+    device = "/dev/disk/by-uuid/da34b822-6352-4eb3-9db5-f8418342e597";
     fsType = "auto";
     options = [ "defaults" "user" "rw" "noauto" ];
   };
