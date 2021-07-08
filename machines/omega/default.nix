ce46fee076a9290fd6b1645a71af18c5fa462769
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-07-08 11:08:08 +0200

    add properconfiguration for thinkpad

--- machines/omega/default.nix
+++ machines/omega/default.nix
@@ -10,17 +10,19 @@ let
   '';
 in
   {
-    imports = [
-      ../../hardware/usb/backup
-      ../../hardware/usb/cdrom
-      ../../hardware/usb/secret
+    imports =
+      [ (modulesPath + "/installer/scan/not-detected.nix")
+        ../../hardware/usb/backup
+        ../../hardware/usb/cdrom
+        ../../hardware/usb/secret
 
-      ../../roles/common
-      ../../roles/entertainment
-      ../../roles/workstation
-      ../../roles/xserver
-      ../../users/placek.nix
-    ];
+        ../../roles/common
+        ../../roles/entertainment
+        ../../roles/workstation
+        ../../roles/xserver
+
+        ../../users/placek.nix
+      ];
 
     fileSystems."/" =
       { device = "/dev/disk/by-label/nixos";
@@ -28,66 +30,47 @@ in
       };
 
     fileSystems."/boot" =
-      { device = "/dev/disk/by-label/BOOT";
+      { device = "/dev/disk/by-label/boot";
         fsType = "vfat";
       };
 
     swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
 
-    #boot.extraModulePackages                            = [ ];
-    #boot.initrd.availableKernelModules                  = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-    #boot.initrd.kernelModules                           = [ "amdgpu" ];
-    #boot.kernelModules                                  = [ "kvm-intel" "i915" ];
-    #hardware.cpu.intel.updateMicrocode                  = lib.mkDefault config.hardware.enableRedistributableFirmware;
-    #boot.kernelPackages                                 = pkgs.linuxPackages_5_11;
-    #boot.kernelParams                                   = [ "acpi_backlight=vendor" "video.use_native_backlight=1" ];
-    #boot.loader.efi.canTouchEfiVariables                = true;
-    #boot.loader.systemd-boot.enable                     = true;
-    #hardware.bluetooth.enable                           = true;
-    #hardware.enableRedistributableFirmware              = true;
-    #hardware.facetimehd.enable                          = true;
-    #hardware.opengl.driSupport                          = true;
-    #hardware.opengl.enable                              = true;
-    #hardware.opengl.extraPackages                       = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver ];
-    #hardware.pulseaudio.enable                          = true;
-    #hardware.pulseaudio.extraModules                    = [ pkgs.pulseaudio-modules-bt ];
-    #hardware.pulseaudio.package                         = pkgs.pulseaudioFull;
-    #hardware.video.hidpi.enable                         = lib.mkDefault true;
-    #networking.firewall.allowPing                       = false;
-    #networking.firewall.allowedTCPPortRanges            = [ { from = 3000; to = 3009; } ];
-    #networking.firewall.enable                          = true;
-    #networking.hostName                                 = "omega";
-    #powerManagement.cpuFreqGovernor                     = lib.mkDefault "performance";
-    #programs.light.enable                               = true;
-    #services.mbpfan.enable                              = true;
-    #services.mbpfan.highTemp                            = 70;
-    #services.mbpfan.lowTemp                             = 60;
-    #services.mbpfan.maxTemp                             = 80;
-    #services.xserver.libinput.enable                    = true;
-    #services.xserver.libinput.touchpad.naturalScrolling = true;
-    #services.xserver.libinput.touchpad.scrollMethod     = "twofinger";
-    #services.xserver.libinput.touchpad.tapping          = false;
-    #services.xserver.resolutions                        = [
-    #  { x = 1920; y = 1200; }
-    #  { x = 1920; y = 1080; }
-    #];
+    boot.extraModulePackages                            = with config.boot.kernelPackages; [ acpi_call ];
+    boot.initrd.availableKernelModules                  = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    boot.initrd.kernelModules                           = [ "i915" ];
+    boot.kernelModules                                  = [ "acpi_call" ];
+    boot.loader.efi.canTouchEfiVariables                = true;
+    boot.loader.systemd-boot.enable                     = true;
+    environment.systemPackages                          = [ nvidia-offload ];
+    hardware.bluetooth.enable                           = true;
+    hardware.cpu.intel.updateMicrocode                  = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    hardware.opengl.driSupport                          = true;
+    hardware.opengl.enable                              = true;
+    hardware.opengl.extraPackages                       = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver ];
+    hardware.pulseaudio.enable                          = true;
+    hardware.pulseaudio.extraModules                    = [ pkgs.pulseaudio-modules-bt ];
+    hardware.pulseaudio.package                         = pkgs.pulseaudioFull;
+    hardware.video.hidpi.enable                         = lib.mkDefault true;
+    networking.firewall.allowPing                       = false;
+    networking.firewall.allowedTCPPortRanges            = [ { from = 3000; to = 3009; } ];
+    networking.firewall.enable                          = true;
+    networking.hostName                                 = "omega";
+    powerManagement.cpuFreqGovernor                     = lib.mkDefault "performance";
+    programs.light.enable                               = true;
+    services.throttled.enable                           = lib.mkDefault true;
+    services.xserver.libinput.enable                    = true;
+    services.xserver.libinput.touchpad.naturalScrolling = true;
+    services.xserver.libinput.touchpad.scrollMethod     = "twofinger";
+    services.xserver.libinput.touchpad.tapping          = false;
+    services.xserver.resolutions                        = [ { x = 1920; y = 1080; } ];
+    services.xserver.videoDrivers                       = lib.mkDefault [ "nvidia" ];
 
-    # thinkpad specific - hopefully
-    boot.initrd.kernelModules = [ "i915" ];
-    boot.kernelModules = [ "acpi_call" ];
-    boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
-    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-    hardware.opengl.extraPackages = with pkgs; [ vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver ];
-    services.throttled.enable = lib.mkDefault true;
-
-    # nvidia
-    #services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
-    #environment.systemPackages = [ nvidia-offload ];
-
-    #hardware.nvidia.prime = {
-    #  offload.enable = lib.mkDefault true;
-    #  # Hardware should specify the bus ID for intel/nvidia devices
-    #};
+    hardware.nvidia.prime = {
+      offload.enable = lib.mkDefault true;
+      intelBusId  = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
 
     users.users.placek.packages = with pkgs; [
       arduino
