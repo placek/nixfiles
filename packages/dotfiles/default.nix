f846ccb63c67954599ba164b0ae5a0662773dcce
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-08-24 08:04:02 +0200

    fixes

--- packages/dotfiles/default.nix
+++ packages/dotfiles/default.nix
@@ -7,8 +7,8 @@ pkgs.stdenv.mkDerivation rec {
   src = pkgs.fetchFromGitHub {
     owner  = "placek";
     repo   = "dotfiles";
-    rev    = "17e31828aeb829a8b5477e0773f106536159b5ba";
-    sha256 = "0g043fjah5qdxwn0q3r3cq7whisx4h2w37mpipkkfcn43hwkh31s";
+    rev    = "d5464449e069720fcffe79435e2ced7d212535dd";
+    sha256 = "1kd56209yg3i8vvifyz8h4j0cq2ixf31z9q8m7pw032qqvln8ypr";
   };
 
   buildInputs  = [ pkgs.haskellPackages.mustache ];
