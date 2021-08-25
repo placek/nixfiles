a6183ff1e07620b0d0df73079e9a7acaf7b0add6
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-08-25 15:25:55 +0200

    add nix shell indicator

--- packages/dotfiles/default.nix
+++ packages/dotfiles/default.nix
@@ -7,8 +7,8 @@ pkgs.stdenv.mkDerivation rec {
   src = pkgs.fetchFromGitHub {
     owner  = "placek";
     repo   = "dotfiles";
-    rev    = "9a10ea919c149345edb1e40e5762ef3bcad3dd13";
-    sha256 = "1d75c8ry6lry67jcnsqdnyd9f7xxalhm2pc0dj2gakpppqrzn73f";
+    rev    = "408bcbb154f10aba1292aa903e47a55aabb17608";
+    sha256 = "12xvay79mmmrkryx9sxxnqqxfnczmyh4855l3yrniqh3wplvvrgk";
   };
 
   buildInputs  = [ pkgs.haskellPackages.mustache ];
