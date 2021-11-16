bfb119efb816e87a1aac230178990c6052a4ae4d
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-11-16 13:23:52 +0100

    add package update script

--- packages/dotfiles/default.nix
+++ packages/dotfiles/default.nix
@@ -1,20 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation rec {
-  pname   = "dotfiles";
-  version = builtins.substring 0 6 src.rev;
+let
+  pin  = pkgs.lib.importJSON ./source.json;
+in
+  pkgs.stdenv.mkDerivation rec {
+    pname   = "dotfiles";
+    version = builtins.substring 0 6 src.rev;
 
-  src = pkgs.fetchFromGitHub {
-    owner  = "placek";
-    repo   = "dotfiles";
-    rev    = "eb5ca0eb6ce75d3456a46149a70102c7e94bd04b";
-    sha256 = "0xwic6086yp6s7f9a1qi6y5sqhbw2pds0l200hzzq7xsv5kj93wm";
-  };
+    src = pkgs.fetchFromGitHub {
+      owner = "placek";
+      repo  = "dotfiles";
+      inherit (pin) rev sha256;
+    };
 
-  buildInputs  = [ pkgs.haskellPackages.mustache ];
-  buildPhase   = "";
-  installPhase = ''
-    mkdir -p $out
-    cp -r * $out
-  '';
-}
+    buildInputs  = [ pkgs.haskellPackages.mustache ];
+    buildPhase   = "";
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out
+    '';
+  }
