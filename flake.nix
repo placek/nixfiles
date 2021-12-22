11932d6a28071c40069ddcc6ee6061e43ef80243
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-12-22 07:43:18 +0100

    [flakes] apply proper inputs to machines

--- flake.nix
+++ flake.nix
@@ -2,17 +2,18 @@
   description = "The configuration flakes for my personal setup of NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/d235056d6d6dcbd2999bd55fd120d831d4df6304";
+    omega_nixpkgs.url  = "github:NixOS/nixpkgs/9e86f5f7a19db6da2445f07bafa6694b556f9c6d";
+    lambda_nixpkgs.url = "github:NixOS/nixpkgs/d235056d6d6dcbd2999bd55fd120d831d4df6304";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, omega_nixpkgs, lambda_nixpkgs, ... }: {
     nixosConfigurations = {
-      omega = nixpkgs.lib.nixosSystem {
+      omega = omega_nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./machines/omega ];
       };
 
-      lambda = nixpkgs.lib.nixosSystem {
+      lambda = lambda_nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./machines/lambda ];
       };
