7215f6ba478ecc393d7d60cafa8a0b018bd61a49
Paweł Placzyński <placzynski.pawel@gmail.com>
2021-12-23 14:04:45 +0100

    [flake] move custom-fonts to flake inputs

--- flake.nix
+++ flake.nix
@@ -6,9 +6,10 @@
     lambda_nixpkgs.url   = "github:NixOS/nixpkgs/d235056d6d6dcbd2999bd55fd120d831d4df6304";
     dotfiles_flake.url   = "github:placek/dotfiles/master";
     wallpapers_flake.url = "github:placek/wallpapers/master";
+    fonts_flake.url      = "github:placek/custom-fonts/master";
   };
 
-  outputs = { self, omega_nixpkgs, lambda_nixpkgs, dotfiles_flake, wallpapers_flake, ... }:
+  outputs = { self, omega_nixpkgs, lambda_nixpkgs, dotfiles_flake, wallpapers_flake, fonts_flake, ... }:
     let
       system = "x86_64-linux";
       pkgs   = import omega_nixpkgs {
@@ -17,7 +18,7 @@
         config.packageOverrides = pkgs: rec {
           custom.dotfiles     = dotfiles_flake.defaultPackage.x86_64-linux;
           custom.wallpapers   = wallpapers_flake.defaultPackage.x86_64-linux;
-          custom.custom-fonts = pkgs.callPackage ./packages/custom-fonts {};
+          custom.custom-fonts = fonts_flake.defaultPackage.x86_64-linux;
           custom.dcc6502      = pkgs.callPackage ./packages/dcc6502 {};
           custom.minipro      = pkgs.callPackage ./packages/minipro {};
           custom.vasm         = pkgs.callPackage ./packages/vasm {};
