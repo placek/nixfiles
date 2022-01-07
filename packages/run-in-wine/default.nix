79f49243d596d9f544ae2b47027e94cff9977502
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-01-07 13:54:58 +0100

    [entertainment] add RomeTW launch script

---
+++ packages/run-in-wine/default.nix
@@ -0,0 +1,17 @@
+  { pkgs, name, cpath, exe, prefix }:
+  pkgs.stdenv.mkDerivation {
+    name = "wine-${name}";
+    src = builtins.toFile "wine-${name}.sh" ''
+      #!/usr/bin/env bash
+      cd "${prefix}/drive_c/${cpath}"
+      exec wine "${exe}"
+    '';
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/wine-${name}
+      chmod +x $out/bin/wine-${name}
+    '';
+    buildInputs = [];
+    propagatedBuildInputs = [ pkgs.wineWowPackages.stable ];
+  }
--- roles/entertainment/default.nix
+++ roles/entertainment/default.nix
@@ -1,11 +1,21 @@
 { config, pkgs, ... }:
+let
+  wine-rome-total-war = import ../../packages/run-in-wine {
+    inherit pkgs;
+    prefix = "$HOME/.wine";
+    name   = "rome-total-war";
+    cpath  = "Program Files (x86)/The Creative Assembly/Rome - Total War";
+    exe    = "RomeTW.exe";
+  };
+in
+  {
+    hardware.opengl.driSupport32Bit = true;
+    environment.systemPackages = with pkgs; [
+      glxinfo
+      libretro.mesen
+      mesa
+      wineWowPackages.stable
 
-{
-  hardware.opengl.driSupport32Bit = true;
-  environment.systemPackages = with pkgs; [
-    glxinfo
-    libretro.mesen
-    mesa
-    wineWowPackages.stable
-  ];
-}
+      wine-rome-total-war
+    ];
+  }
