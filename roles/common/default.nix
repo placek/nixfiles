bfa04d0c1bac7fad676abec56b956be316307d32
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-03-23 09:49:10 +0100

    allow wheel to use paswswordless sudo

--- roles/common/default.nix
+++ roles/common/default.nix
@@ -1,26 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  boot.cleanTmpDir               = true;
-  boot.consoleLogLevel           = 0;
-  boot.supportedFilesystems      = [ "ntfs" ];
-  console.keyMap                 = "pl";
-  hardware.keyboard.zsa.enable   = true;
-  i18n.defaultLocale             = "pl_PL.UTF-8";
-  nix.gc.automatic               = true;
-  nix.gc.options                 = "--delete-older-than 7d";
-  nix.useSandbox                 = true;
-  nixpkgs.config.allowUnfree     = true;
-  services.cron.enable           = true;
-  services.pcscd.enable          = true;
-  services.printing.enable       = true;
-  services.printing.drivers      = [ pkgs.foo2zjs ];
-  services.udev.packages         = [ pkgs.yubikey-personalization ];
-  system.autoUpgrade.allowReboot = true;
-  system.autoUpgrade.channel     = https://nixos.org/channels/nixos-21.11;
-  system.autoUpgrade.enable      = true;
-  system.stateVersion            = "21.11";
-  time.timeZone                  = "Europe/Warsaw";
+  boot.cleanTmpDir                 = true;
+  boot.consoleLogLevel             = 0;
+  boot.supportedFilesystems        = [ "ntfs" ];
+  console.keyMap                   = "pl";
+  hardware.keyboard.zsa.enable     = true;
+  i18n.defaultLocale               = "pl_PL.UTF-8";
+  nix.gc.automatic                 = true;
+  nix.gc.options                   = "--delete-older-than 7d";
+  nix.useSandbox                   = true;
+  nixpkgs.config.allowUnfree       = true;
+  services.cron.enable             = true;
+  services.pcscd.enable            = true;
+  services.printing.enable         = true;
+  services.printing.drivers        = [ pkgs.foo2zjs ];
+  services.udev.packages           = [ pkgs.yubikey-personalization ];
+  security.sudo.wheelNeedsPassword = false;
+  system.autoUpgrade.allowReboot   = true;
+  system.autoUpgrade.channel       = https://nixos.org/channels/nixos-21.11;
+  system.autoUpgrade.enable        = true;
+  system.stateVersion              = "21.11";
+  time.timeZone                    = "Europe/Warsaw";
 
   # flakes
   nix = {
@@ -45,8 +46,6 @@
       bind -T copy-mode-vi v send-keys -X begin-selection
       bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
       bind a run "tmux split-pane 'projects add'"
-      bind-key -n End send Escape "OF"
-      bind-key -n Home send Escape "OH"
       set  -g display-time 2000
       set  -g focus-events on
       set  -g mouse on
