86e7788116b6afbebe94c7d05c0ef53a671c8f0c
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-12-01 08:25:54 +0100

    add obelisk binary cache

--- roles/workstation/default.nix
+++ roles/workstation/default.nix
@@ -13,10 +13,10 @@
   virtualisation.docker.enable          = true;
   virtualisation.libvirtd.enable        = true;
 
-  # plutus binary cache
+  # binary cache
   nix = {
-    settings.substituters = [ "https://hydra.iohk.io" "https://iohk.cachix.org" "https://cache.iog.io" ];
-    settings.trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
+    settings.substituters = [ "https://hydra.iohk.io" "https://iohk.cachix.org" "https://cache.iog.io" "https://nixcache.reflex-frp.org" ];
+    settings.trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
   };
 
   services.acpid.enable = true;
